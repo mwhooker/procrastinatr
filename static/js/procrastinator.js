9b5519b014c62b903609afd4672d2e1b4689c8e7
Matthew Hooker <mwhooker@gmail.com>
2011-06-25 01:11:48 -0400

    subreddit Backbone code

--- static/js/procrastinator.js
+++ static/js/procrastinator.js
@@ -2,21 +2,47 @@
     
 
     var opiateTpl = "<div> \
-    <a href='http://reddit.com<%= permalink %>' target='none'><%= title %></a> \
-    <div class='clear'></div> \
-    <a href='<%= image_url %>'><img src='<%= image_url %>'></img></a> \
-    </div>";
+        <a href='http://reddit.com<%= permalink %>' target='none'><%= title %></a> \
+        <div class='clear'></div> \
+        <a href='<%= image_url %>'><img src='<%= image_url %>'></img></a> \
+        </div>";
+
+    var subredditTpl = "<input type='checkbox' name='<%= subreddit %>' \
+        id='<%= subreddit %>' <% if (checked) { print('checked') } %> /> \
+        <label for='<%= subreddit %>'>/r/<%= subreddit %></label>";
+
+    window.Subreddit = Backbone.Model.extend({});
+
+    window.SubredditList = Backbone.Collection.extend({
+        model: window.Subreddit
+    });
+
+    window.SubredditView = Backbone.View.extend({
+        tagName: 'li',
+
+        template: _.template(subredditTpl),
+
+        initialize: function() {
+            _.bindAll(this, 'render');
+            this.model.bind('change', this.render);
+            this.model.view = this;
+        },
+
+        render: function() {
+            $(this.el).html(this.template(this.model.toJSON()));
+            return this;
+        },
+    });
+
 
     window.Opiate = Backbone.Model.extend({});
 
     window.OpiateList = Backbone.Collection.extend({
         model: window.Opiate
-    })
+    });
 
     window.OpiateView = Backbone.View.extend({
         tagName: 'li',
-
-        maxWidth: $('#content').width(),
 
         template: _.template(opiateTpl),
 
@@ -33,22 +59,27 @@
     });
 
     window.Procrastinator = Backbone.View.extend({
-      el: $('#opiates'),
 
-      initialize: function() {
-        _.bindAll(this, 'addOne');
+        initialize: function() {
+            _.bindAll(this, 'addImage', 'addSubreddit');
+            window.OpiateList.bind('add', this.addImage);
+            window.SubredditList.bind('add', this.addSubreddit);
+        },
 
-        window.OpiateList.bind('add', this.addOne);
+        addImage: function(opiate) {
+            var view = new window.OpiateView({
+                model: opiate,
+                id: opiate.id
+            });
+            $('#opiates').append(view.render().el);
+        },
 
-      },
-
-      addOne: function(opiate) {
-        var view = new window.OpiateView({
-            model: opiate,
-            id: opiate.id
-        });
-        $('#opiates').append(view.render().el);
-      },
+        addSubreddit: function(subreddit) {
+            var view = new window.SubredditView({
+                model: subreddit
+            });
+            $('#options').append(view.render().el);
+        }
 
     });
 
@@ -56,15 +87,23 @@
         var socket = new io.Socket();
         window.App = new window.Procrastinator;
 
+        _.each(['pics', 'funny', 'comics', 'inglip'], function(item) {
+            var subreddit = new window.Subreddit({
+                'subreddit': item,
+                'checked': false
+            });
+            window.App.addSubreddit(subreddit);
+        });
+
         socket.on('connect', function(){ 
             console.log('connected');
         }) 
         socket.on('message', function(data){ 
             var opiate = new window.Opiate(data);
-            window.App.addOne(opiate);
+            window.App.addImage(opiate);
         })
         socket.on('disconnect', function(){})
         socket.connect();
     }
-    main();
+    $(document).ready(main);
 })();
